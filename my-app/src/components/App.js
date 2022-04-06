2136609ebebfa2f1f817928fabec4c4caa5f938c
TenoriousOz <lucasprofessor.se@gmail.com>
2022-04-05 23:49:22 -0300

    tentativa de adicionar as routes para as paginas

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,6 +1,7 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../App.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./NavBar";
 import MyProjects from "./MyProjects";
 import MyTasks from "./MyTasks";
@@ -9,12 +10,15 @@ import Chart from "./Charts";
 
 function App() {
   return (
-    <div>
-    <NavBar/>
-
-      <div className="App">
-        {/* <Route path="/" element={<MyProjects />} /> */}
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<MyProjects />} />
+          <Route path="/mytasks" element={<MyTasks />} />
+          <Route path="/chart" element={<Chart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
