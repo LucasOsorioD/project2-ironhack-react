8b4cc749c8974b88b6b65f5eda603f055639b62a
Nathalia Maia <nathaliamaiape@gmail.com>
2022-04-09 09:50:31 -0300

    removed coments

--- my-app/src/components/MyProjects.js
+++ my-app/src/components/MyProjects.js
@@ -167,16 +167,3 @@ function MyProjects() {
 }
 export default MyProjects;
 
-//Card do projeto-
-
-//Pegar o total de tasks do projeto e colocar no card;
-//O status do card é baseado em três coisas:
-//1. Status "completo" será criado quando todas as tasks estiverem finalizadas;
-//2. Status "pendente" acontecerá ao usuário selecionar a opção "stop project";
-//3. Status "active" acontecerá quando o card de projeto for criado(estado inicial);
-
-//criar gráfico de status
-// emojis :)
-
-// aba de delete, edit e stop ou restart project
-//pensar sobre os contribuidores.
