1b8f84df8de592bb25f9665e48302958182342c4
Nathalia Maia <nathaliamaiape@gmail.com>
2022-04-10 00:22:14 -0300

    desconsiderar esse commit

---
+++ my-app/src/components/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function Api() {
+  const [projectObj, setProjectObj] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+    try {
+      const response = await axios.get(
+        `https://ironrest.herokuapp.com/cardinator/`
+      );
+      setProjectObj([...response.data]);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  console.log("hello, Brasil");
+  return <div></div>;   
+  },[])
+
+}
+export default Api;
--- my-app/src/components/MyProjects.js
+++ my-app/src/components/MyProjects.js
@@ -1,62 +1,110 @@
 import Card from "react-bootstrap/Card";
 import Charts from "./Charts";
 import Chart from "chart.js/auto";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Graph from "./Graph";
 
 function MyProjects() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const ctx = document.getElementById("myChart");
 
-    const myChart = new Chart(ctx, {
-      type: "doughnut",
-      options: { responsive: true, maintainAspectRatio: false },
-      data: {
-        datasets: [
-          {
-            data: [70, 30],
-            fill: true,
-            borderColor: "#EAEAEA",
-            backgroundColor: ["#F9c262", "#EAEAEA"],
-            tension: 0.1,
-          },
-        ],
-      },
-    });
+  const navigate = useNavigate();
+  const [projectObj, setProjectObj] = useState([]);
+  const [charts, setCharts] = useState(null);
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `https://ironrest.herokuapp.com/cardinator/`
+        );
+        setProjectObj([...response.data]);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchData();
+
   }, []);
 
-  return (
-    <div style={{ marginLeft: "12vh", marginTop: "12vh" }}>
-      <Card
-        className="shadow"
-        style={{ width: "18rem", borderRadius: "1rem" }}
-        onClick={() => navigate("/mytasks")}
-      >
-        <Card.Header
-          style={{
-            backgroundColor: "#F9C262",
-            borderTopRightRadius: "1rem",
-            borderTopLeftRadius: "1rem",
-            paddingTop: "1.5vh",
-            paddingBottom: "1.5vh",
-          }}
-          as="h5"
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+      setCharts((grafico) => {
+        console.log(grafico);
+        if (grafico) {
+          grafico.destroy();
+        }
+        // ^ o .destroy serve para remover o 'chart' antigo para dar espaço para o novo
+        const myChart = new Chart(ctx, {
+          type: "doughnut",
+          options: { responsive: true, maintainAspectRatio: false },
+          data: {
+            datasets: [
+              {
+                data: [0, 7],
+                fill: true,
+                borderColor: "#EAEAEA",
+                backgroundColor: ["#F9c262", "#EAEAEA"],
+                tension: 0.1,
+              },
+            ],
+          },
+        });
+        return myChart;
+        //para criar um novo grafico chamo a const após destruir o state anterior, usando o return.
+      });
+    }
+  }, [projectObj]);
+
+return (
+  <div style={{ marginLeft: "12vh", marginTop: "12vh" }}>
+    {projectObj.map((items) => {
+      return (
+        <Card
+          className="shadow"
+          style={{ width: "18rem", borderRadius: "1rem" }}
+          onClick={() => navigate("/mytasks")}
+          key={items._id}
         >
-          <strong>#project name</strong>
-        </Card.Header>
-        <Card.Body style={{ display: "flex", flexDirection: "row" }}>
-          <div
+          <Card.Header
             style={{
-              position: "relative",
-              width: "500px",
-              marginTop: "4rem",
-              bottom: "2rem",
+              backgroundColor: "#F9C262",
+              borderTopRightRadius: "1rem",
+              borderTopLeftRadius: "1rem",
+              paddingTop: "1.5vh",
+              paddingBottom: "1.5vh",
             }}
+            as="h5"
           >
-            <canvas id="myChart" width="150" height="150"></canvas>
-          </div>
-          <Card.Text>
+            <strong>{items.projectName}</strong>
+          </Card.Header>
+          <Card.Body style={{ display: "flex", flexDirection: "row" }}>
+            <div
+              style={{
+                position: "relative",
+                width: "500px",
+                marginTop: "4rem",
+                bottom: "2rem",
+              }}
+            >
+              <Graph
+                data={{
+                  datasets: [
+                    {
+                      data: [2, 5],
+                      //esse é o valor que efetivamente está sendo refletido no grafico do projeto na home
+                      fill: true,
+                      borderColor: "#EAEAEA",
+                      backgroundColor: ["#F9c262", "#EAEAEA"],
+                      tension: 0.1,
+                    },
+                  ],
+                }}
+                //esse 'Graph' foi a forma que deu para fazer migrando o nosso canvas para um elemento a parte (ate fica mais organizado)
+              />
+            </div>
             <div
               style={{
                 display: "flex",
@@ -76,7 +124,7 @@ function MyProjects() {
                 }}
               >
                 <p style={{ color: "#515151" }} className="mb-1">
-                  2
+                  {items.contributors}
                 </p>
 
                 <hr
@@ -99,7 +147,20 @@ function MyProjects() {
                 }}
               >
                 <p style={{ color: "#515151" }} className="mb-1">
-                  150
+                  {items.totalAmountTasks}
+                </p>
+
+                <p style={{ color: "#F9c262" }}> total of tasks</p>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "flex-end",
+                  flexDirection: "column",
+                }}
+              >
+                <p style={{ color: "#515151" }} className="mb-1" id="status">
+                  active
                 </p>
 
                 <hr
@@ -122,7 +183,7 @@ function MyProjects() {
                 }}
               >
                 <p style={{ color: "#515151" }} className="mb-1" id="status">
-                  active
+                  {items.status}
                 </p>
 
                 <hr
@@ -145,7 +206,7 @@ function MyProjects() {
                 }}
               >
                 <p style={{ color: "#515151" }} className="mb-1">
-                  70 %
+                  {items.workProgress}
                 </p>
 
                 <hr
@@ -164,11 +225,15 @@ function MyProjects() {
                 </p>
               </div>
             </div>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
-  );
+          </Card.Body>
+        </Card>
+      );
+    })}
+  </div>
+);
+
 }
+  
+
 export default MyProjects;
 
